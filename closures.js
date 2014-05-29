bbed6e871c3362348e059b93ccb6e4d5e1d7dfab
Kevin Sivabalan <kevin@buzzbuzzhome.com>
2014-05-29 16:54:06 -0400

    at interstellar negibourhood

--- closures.js
+++ closures.js
@@ -11,38 +11,54 @@ var outsideUniverse = "I'm Outside the Known Universe";
   }
 
   var virgoSuperCluster = function (person2,cb) {
-    var pIVSC = person2;
+    var pLSC = person2;
     var personIVSC = "I'm in the Virgo Super Cluster";
     return cb(personIVSC);
   }
 
-  var 
+  var localGalacticGroup = function (person3,cb) {
+    var pIVSC = person3;
+    var personLGG = "I'm in the Local Galactic Group";
+    return cb(personLGG);
+  }
+
+
+  var enterMilkyWay = function(personInLocalGalacticGroup){
+    var _personInLocalGalacticGroup = personInLocalGalacticGroup;
+
+    var milkyWayGalaxy = function(person4, cb){
+      var pILGG = person4;
+      var personIMWG = "I'm in the Milky Way Galaxy";
+      return cb(personIMWG);
+    }
+
+    milkyWayGalaxy(personInLocalGalacticGroup, function(personInMilkyWayGalaxy){
+      var _personInMilkyWayGalaxy = personInMilkyWayGalaxy;
+
+      
+      solarInterstellarNeighbourHood(personInMilkyWayGalaxy, function(personInSolarInterstellarNeighbourHood){
+        var _personInSolarInterstellarNeighbourHood = personInSolarInterstellarNeighbourHood;
+        solarSystem(personInSolarInterstellarNeighbourHood, function(personInSolarSystem){
+          var _personInSolarSystem = personInSolarSystem;
+          earth(personInSolarSystem,function(personOnEarth){
+            var _personOnEarth = personOnEarth;
+
+            debugger;
+
+
+
+          })
+        })
+      })
+    })
+  }
 
 
   localSuperCluster(personInUniverse,function(personInLocalSuperCluster){
     var _personInLocalSuperCluster = personInLocalSuperCluster;
     virgoSuperCluster(personInLocalSuperCluster, function (personInVirgoSuperCluster) {
       var _personInVirgoSuperCluster = personInVirgoSuperCluster;
-      localGalacticGroup(personInVirgoSuperCluster, function(personInLocalGalacticGroup){
-        var _personInLocalGalacticGroup = personInLocalGalacticGroup;
-        milkyWayGalaxy(personInLocalGalacticGroup, function(personInMilkyWayGalaxy){
-          var _personInMilkyWayGalaxy = personInMilkyWayGalaxy;
-          solarInterstellarNeighbourHood(personInMilkyWayGalaxy, function(personInSolarInterstellarNeighbourHood){
-            var _personInSolarInterstellarNeighbourHood = personInSolarInterstellarNeighbourHood;
-            solarSystem(personInSolarInterstellarNeighbourHood, function(personInSolarSystem){
-              var _personInSolarSystem = personInSolarSystem;
-              earth(personInSolarSystem,function(personOnEarth){
-                var _personOnEarth = personOnEarth;
-
-                debugger;
-
-
-
-              })
-            })
-          })
-        })
-      })
+      localGalacticGroup(personInVirgoSuperCluster, enterMilkyWay)
     })
   })
 
