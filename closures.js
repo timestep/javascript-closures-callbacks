ac433494aefac1fa517df4e68949b04a10e83798
Kevin Sivabalan <kevin@buzzbuzzhome.com>
2014-05-30 10:59:22 -0400

    fail

--- closures.js
+++ closures.js
@@ -4,30 +4,19 @@ outsideUniverse = "I'm Outside the Known Universe";
 
   var personInUniverse = "I'm somewhere in the universe";
 
-  var localSuperCluster = function (person1,cb) {
-    var pIU = person1;
-    var personILSC = "I'm somewhere in the Local Super Cluster";
-    return cb(personILSC);
-  }
-
   var virgoSuperCluster = function (person2,cb) {
     var pLSC = person2;
     var personIVSC = "I'm in the Virgo Super Cluster";
-    return cb(personIVSC);
+    cb(personIVSC);
   }
 
   var localGalacticGroup = function (person3,cb) {
     var pIVSC = person3;
     var personLGG = "I'm in the Local Galactic Group";
-    return cb(personLGG);
+    cb(personLGG);
   }
 
-  var earth = function(person7, cb){
-    debugger;
-    var pSS = person7;
-    var personE = "I'm on Earth";
-    return cb(personE);
-  }
+
 
   var enterMilkyWay = function(personInLocalGalacticGroup){
     var _personInLocalGalacticGroup = personInLocalGalacticGroup;
@@ -35,7 +24,7 @@ outsideUniverse = "I'm Outside the Known Universe";
     var milkyWayGalaxy = function(person4, cb){
       var pILGG = person4;
       var personIMWG = "I'm in the Milky Way Galaxy";
-      return cb(personIMWG);
+      cb(personIMWG);
     }
 
     milkyWayGalaxy(personInLocalGalacticGroup, function(personInMilkyWayGalaxy){
@@ -44,33 +33,46 @@ outsideUniverse = "I'm Outside the Known Universe";
       var solarInterstellarNeighbourHood = function (person5,cb) {
         var pIMWG = person5;
         var personISIN = "I'm in the Solar InterStellar Neighbourhood";
-        return cb(personISIN);
+        cb(personISIN);
       }
 
       var solarSystem = function(person6,cb){
         var pISIN = person6;
         var personSS = "I'm in the Solar System";
-        return cb(personSS);
+        cb(personSS);
       }
 
       solarInterstellarNeighbourHood(personInMilkyWayGalaxy, function(personInSolarInterstellarNeighbourHood){
         var _personInSolarInterstellarNeighbourHood = personInSolarInterstellarNeighbourHood;
         solarSystem(personInSolarInterstellarNeighbourHood, function(personInSolarSystem){
           var _personInSolarSystem = personInSolarSystem;
-          earth(personInSolarSystem,function(personOnEarth){
-            var _personOnEarth = personOnEarth;
 
 
-            //nothing is accessable here.
-            // only personOnEarth;
-            debugger;
+          earth(personInSolarSystem);
 
-          });
+          function earth(person7){
+            var pSS = person7;
+            var personE = "I'm on Earth";
+            var personOnEarth = personE;
+              var _personOnEarth = personOnEarth;
+
+        debugger;
+
+              //nothing is accessable here.
+              // only personOnEarth;
+          }
+
+
         });
       });
     });
   }
 
+  var localSuperCluster = function (person1,cb) {
+    var pIU = person1;
+    var personILSC = "I'm somewhere in the Local Super Cluster";
+    cb(personILSC);
+  }
 
   localSuperCluster(personInUniverse,function(personInLocalSuperCluster){
     var _personInLocalSuperCluster = personInLocalSuperCluster;
